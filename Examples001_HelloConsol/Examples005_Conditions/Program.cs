4be9688782cd73a86d936c65e31c88f10a71549b
axan <akanbaimuratov96@icloud.com>
2023-04-10 05:20:36 +0600

    Examples 005 Conditions

---
+++ Examples001_HelloConsol/Examples005_Conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Ахан")
+{
+  Console.WriteLine("Ура это же АХАН!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
